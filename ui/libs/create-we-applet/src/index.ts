2dc3535c49fa012d4165302bda17e0a56d74088c
Guillem Cordoba <guillem.cordoba@gmail.com>
2022-06-28 14:35:03 +0200

    Generator working

--- ui/libs/create-we-applet/src/index.ts
+++ ui/libs/create-we-applet/src/index.ts
@@ -1,28 +1,8 @@
-// eslint-disable-next-line @typescript-eslint/ban-ts-comment
-//@ts-ignore
-import semver from "semver";
-import chalk from "chalk";
 import { writeDirectoryTree } from "@source-craft/fs";
 import generate from "./generator";
 import { upperFirst, camelCase } from "lodash-es";
 
 console.log(`@lightningrodlabs/create-we-applet`);
-
-if (!semver.gte(process.version, "14.0.0")) {
-  console.log(
-    chalk.bgRed("\nUh oh! Looks like you dont have Node v14 installed!\n")
-  );
-  console.log(`You can do this by going to ${chalk.underline.blue(
-    `https://nodejs.org/`
-  )}
-  Or if you use nvm:
-    $ nvm install node ${chalk.gray(
-      `# "node" is an alias for the latest version`
-    )}
-    $ nvm use node
-  `);
-  process.exit(1);
-}
 
 if (!process.argv[2]) {
   console.log(
