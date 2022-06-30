18e1a8e6a5cbb5c6dd2a47fab9573e2ae83817a9
Guillem Cordoba <guillem.cordoba@gmail.com>
2022-06-30 19:34:35 +0200

    Fixed package

--- ui/app/rollup.config.js
+++ ui/app/rollup.config.js
@@ -2,15 +2,11 @@ import nodeResolve from "@rollup/plugin-node-resolve";
 import typescript from "@rollup/plugin-typescript";
 import commonjs from "@rollup/plugin-commonjs";
 import replace from "@rollup/plugin-replace";
-import builtins from "rollup-plugin-node-builtins";
-import globals from "rollup-plugin-node-globals";
 
 import babel from "@rollup/plugin-babel";
 import html from "@web/rollup-plugin-html";
 import { importMetaAssets } from "@web/rollup-plugin-import-meta-assets";
 import { terser } from "rollup-plugin-terser";
-import { generateSW } from "rollup-plugin-workbox";
-import path from "path";
 
 const HC_PORT = process.env.HC_PORT || 8888;
 const DIST_FOLDER = process.env.HC_PORT ? `.dist/${HC_PORT}` : "dist";
@@ -34,8 +30,6 @@ export default {
     /** Enable using HTML as rollup entrypoint */
     html({
       minify: production,
-      injectServiceWorker: true,
-      serviceWorkerPath: "dist/sw.js",
     }),
     /** Resolve bare module imports */
     nodeResolve({
@@ -47,10 +41,8 @@ export default {
       "process.env.ENV": `"${process.env.ENV}"`,
       "process.env.HC_PORT": `"${HC_PORT}"`,
     }),
-    builtins(),
     typescript({ experimentalDecorators: true, outDir: DIST_FOLDER }),
     commonjs({}),
-    globals(),
     /** Minify JS */
     production && terser(),
     /** Bundle assets references via import.meta.url */
@@ -65,50 +57,13 @@ export default {
           [
             require.resolve("@babel/preset-env"),
             {
-              targets: [
-              ],
+              targets: [],
               modules: false,
               bugfixes: true,
             },
           ],
         ],
-        plugins: [
-          "@babel/plugin-syntax-dynamic-import",
-          [
-            require.resolve("babel-plugin-template-html-minifier"),
-            {
-              modules: {
-                lit: ["html", { name: "css", encapsulation: "style" }],
-              },
-              failOnError: false,
-              strictCSS: true,
-              htmlMinifier: {
-                collapseWhitespace: true,
-                conservativeCollapse: true,
-                removeComments: true,
-                caseSensitive: true,
-                minifyCSS: true,
-              },
-            },
-          ],
-        ],
-      }),
-    /** Create and inject a service worker */
-    production &&
-      generateSW({
-        globIgnores: ["polyfills/*.js", "nomodule-*.js"],
-        navigateFallback: "/index.html",
-        // where to output the generated sw
-        swDest: path.join(DIST_FOLDER, "sw.js"),
-        // directory to match patterns against to be precached
-        globDirectory: path.join(DIST_FOLDER),
-        // cache any html js and css by default
-        globPatterns: ["**/*.{html,js,css,webmanifest}"],
-        skipWaiting: true,
-        clientsClaim: true,
-        runtimeCaching: [
-          { urlPattern: "polyfills/*.js", handler: "CacheFirst" },
-        ],
+        plugins: [],
       }),
   ],
 };
