309d9fbb7f6bc6bddeada0c38c5ec62b8f20b5fb
Matthias <tuttiacc@hotmail.com>
2022-08-17 18:48:35 +0200

    removed unnecessary files

--- ui_dev/app/src/processes/devhub/app-id.ts
+++
@@ -1,4 +0,0 @@
-export function getDevHubAppId(): string {
-  if (process.env.HC_PORT !== undefined) return "DevHub";
-  return "DevHub-0.0.143";
-}
--- ui_dev/app/src/types.ts
+++
@@ -1,6 +0,0 @@
-import {
-  AgentPubKeyB64,
-  HeaderHashB64,
-  EntryHashB64,
-  DnaHashB64,
-} from "@holochain-open-dev/core-types";
