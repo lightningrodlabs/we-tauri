3ca8ba1861257aa402f7e29b4efea2c865811c45
Wesley Finck <wesleyfinck@gmail.com>
2023-03-28 12:07:14 -0600

    include sensemaker store in new client lib

---
+++ client/src/sensemakerService.ts
@@ -0,0 +1,74 @@
+import { AgentPubKey, AppAgentCallZomeRequest, AppAgentClient, EntryHash, Record, RoleName } from '@holochain/client';
+import { AppletConfig, AppletConfigInput, Assessment, ComputeContextInput, CreateAssessmentInput, CulturalContext, Dimension, GetAssessmentsForResourceInput, Method, ResourceDef, RunMethodInput } from './index';
+import { Option } from './utils';
+
+export class SensemakerService {
+  constructor(public client: AppAgentClient, public roleName: RoleName, public zomeName = 'sensemaker') {}
+
+    /**
+   * Get my agentkey, if it has been created
+   * @returns my AgentPubKey
+   */
+    myPubKey(): AgentPubKey {
+      return this.client.myPubKey
+    }
+    
+
+  async createDimension(dimension: Dimension): Promise<EntryHash> {
+    return this.callZome('create_dimension', dimension);
+  }
+
+  async createResourceDef(resourceDef: ResourceDef): Promise<EntryHash> {
+    return this.callZome('create_resource_def', resourceDef);
+  }
+
+  async createAssessment(assessment: CreateAssessmentInput): Promise<EntryHash> {
+    return this.callZome('create_assessment', assessment);
+  }
+
+  async getAssessment(assessmentEh: EntryHash): Promise<Record> {
+    return this.callZome('get_assessment', assessmentEh);
+  }
+
+  async getAssessmentsForResource(getAssessmentsInput: GetAssessmentsForResourceInput): Promise<Array<Assessment>> {
+    return this.callZome('get_assessments_for_resource', getAssessmentsInput);
+  }
+  
+  async createMethod(method: Method): Promise<EntryHash> {
+    return this.callZome('create_method', method);
+  }
+
+  async runMethod(runMethodInput: RunMethodInput): Promise<EntryHash> {
+    return this.callZome('run_method', runMethodInput);
+  }
+
+  async createCulturalContext(culturalContext: CulturalContext): Promise<EntryHash> {
+    return this.callZome('create_cultural_context', culturalContext);
+  }
+
+  async getCulturalContext(culturalContextEh: EntryHash): Promise<Record> {
+    return this.callZome('get_cultural_context', culturalContextEh);
+  }
+
+  async computeContext(computeContextInput: ComputeContextInput): Promise<Array<EntryHash>> {
+    return this.callZome('compute_context', computeContextInput);
+  }
+
+  async checkIfAppletConfigExists(appletName: string): Promise<Option<AppletConfig>> {
+    return this.callZome('check_if_applet_config_exists', appletName);
+  }
+
+  async registerApplet(appletConfig: AppletConfigInput): Promise<AppletConfig> {
+    return this.callZome('register_applet', appletConfig);
+  }
+
+  private callZome(fn_name: string, payload: any) {
+    const req: AppAgentCallZomeRequest = {
+      role_name: this.roleName,
+      zome_name: this.zomeName,
+      fn_name,
+      payload
+    }
+    return this.client.callZome(req);
+  }
+}
---
+++ client/src/sensemakerStore.ts
@@ -0,0 +1,137 @@
+import { AgentPubKey, encodeHashToBase64, EntryHash, Record } from '@holochain/client';
+import { SensemakerService } from './sensemakerService';
+import { AppletConfig, AppletConfigInput, Assessment, ComputeContextInput, CreateAssessmentInput, CulturalContext, Dimension, GetAssessmentsForResourceInput, Method, ResourceDef, RunMethodInput } from './index';
+import { derived, get, Writable, writable } from 'svelte/store';
+import { EntryHashMap } from '@holochain-open-dev/utils'
+import { Option } from './utils';
+
+interface ContextResults {
+  [culturalContextName: string]: EntryHash[],
+}
+export class SensemakerStore {
+  // store any value here that would benefit from being a store
+  // like cultural context entry hash and then the context result vec
+
+  #appletConfig: Writable<AppletConfig> = writable({ dimensions: {}, resource_defs: {}, methods: {}, cultural_contexts: {}, name: "", role_name: "", ranges: {} });
+  #contextResults: Writable<ContextResults> = writable({});
+
+  // TODO: update the structure of this store to include dimension and resource type
+  /*
+  {
+    [resourceEh: string]: Array<Assessment>
+  }
+  */
+  #resourceAssessments: Writable<{ [entryHash: string]: Array<Assessment> }> = writable({});
+
+  /** Static info */
+  public myAgentPubKey: AgentPubKey;
+
+  constructor(
+    protected service: SensemakerService,
+  ) {
+    this.myAgentPubKey = service.myPubKey();
+  }
+
+  resourceAssessments() {
+    return derived(this.#resourceAssessments, resourceAssessments => resourceAssessments)
+  }
+
+  appletConfig() {
+    return derived(this.#appletConfig, appletConfig => appletConfig)
+  }
+
+  contextResults() {
+    return derived(this.#contextResults, contextResults => contextResults)
+  }
+
+  async createDimension(dimension: Dimension): Promise<EntryHash> {
+    const dimensionEh = await this.service.createDimension(dimension);
+    this.#appletConfig.update(appletConfig => {
+      appletConfig.dimensions[dimension.name] = dimensionEh;
+      return appletConfig;
+    });
+    return dimensionEh;
+  }
+
+  async createResourceDef(resourceDef: ResourceDef): Promise<EntryHash> {
+    const resourceDefEh = await this.service.createResourceDef(resourceDef);
+    this.#appletConfig.update(appletConfig => {
+      appletConfig.resource_defs[resourceDef.name] = resourceDefEh;
+      return appletConfig;
+    });
+    return resourceDefEh;
+  }
+
+  async createAssessment(assessment: CreateAssessmentInput): Promise<EntryHash> {
+    const assessmentEh = await this.service.createAssessment(assessment);
+    this.#resourceAssessments.update(resourceAssessments => {
+      const maybePrevAssessments = resourceAssessments[encodeHashToBase64(assessment.resource_eh)];
+      const prevAssessments = maybePrevAssessments ? maybePrevAssessments : [];
+      resourceAssessments[encodeHashToBase64(assessment.resource_eh)] = [...prevAssessments, {...assessment, author: this.myAgentPubKey}]
+      return resourceAssessments;
+    })
+    return assessmentEh;
+  }
+
+  async getAssessment(assessmentEh: EntryHash): Promise<Record> {
+    return await this.service.getAssessment(assessmentEh) 
+  }
+
+  async getAssessmentForResource(getAssessmentsInput: GetAssessmentsForResourceInput): Promise<Array<Assessment>> {
+    const resourceAssessments = await this.service.getAssessmentsForResource(getAssessmentsInput);
+    this.#resourceAssessments.update(resourceAssessmentsPrev => {
+      resourceAssessmentsPrev[encodeHashToBase64(getAssessmentsInput.resource_eh)] = resourceAssessments;
+      return resourceAssessmentsPrev
+    });
+    return resourceAssessments;
+  }
+  
+  async createMethod(method: Method): Promise<EntryHash> {
+    const methodEh = await this.service.createMethod(method);
+    this.#appletConfig.update(appletConfig => {
+      appletConfig.methods[method.name] = methodEh;
+      return appletConfig;
+    });
+    return methodEh;
+  }
+
+  async runMethod(runMethodInput: RunMethodInput): Promise<EntryHash> {
+    return await this.service.runMethod(runMethodInput) 
+  }
+
+  async createCulturalContext(culturalContext: CulturalContext): Promise<EntryHash> {
+    const contextEh = await this.service.createCulturalContext(culturalContext);
+    this.#appletConfig.update(appletConfig => {
+      appletConfig.cultural_contexts[culturalContext.name] = contextEh;
+      return appletConfig;
+    });
+    return contextEh;
+  }
+
+  async getCulturalContext(culturalContextEh: EntryHash): Promise<Record> {
+    return await this.service.getCulturalContext(culturalContextEh) 
+  }
+
+  async computeContext(contextName: string, computeContextInput: ComputeContextInput): Promise<Array<EntryHash>> {
+    const contextResult = await this.service.computeContext(computeContextInput);
+    this.#contextResults.update(contextResults => {
+      contextResults[contextName] = contextResult;
+      return contextResults;
+    });
+    return contextResult;
+  }
+
+  async checkIfAppletConfigExists(appletName: string): Promise<Option<AppletConfig>> {
+    const maybeAppletConfig = await this.service.checkIfAppletConfigExists(appletName);
+    if (maybeAppletConfig) {
+      this.#appletConfig.update(() => maybeAppletConfig)
+    }
+    return maybeAppletConfig;
+  }
+
+  async registerApplet(appletConfigInput: AppletConfigInput): Promise<AppletConfig> {
+    const appletConfig = await this.service.registerApplet(appletConfigInput);
+    this.#appletConfig.update(() => appletConfig);
+    return appletConfig;
+  }
+}
