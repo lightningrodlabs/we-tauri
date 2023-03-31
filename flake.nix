0270d8458bf5d17be3b614b05c93f6c333d5a888
Wesley Finck <wesleyfinck@gmail.com>
2023-03-31 17:19:54 -0600

    switch to nix flakes

---
+++ flake.nix
@@ -0,0 +1,52 @@
+{
+  description = "Template for Holochain app development";
+
+  inputs = {
+    nixpkgs.follows = "holochain/nixpkgs";
+
+    holochain = {
+      url = "github:holochain/holochain";
+      inputs.versions.url = "github:holochain/holochain?dir=versions/0_1";
+    };
+  };
+
+  outputs = inputs @ { ... }:
+    inputs.holochain.inputs.flake-parts.lib.mkFlake
+      {
+        inherit inputs;
+      }
+      {
+        systems = builtins.attrNames inputs.holochain.devShells;
+        perSystem =
+          { config
+          , pkgs
+          , system
+          , ...
+          }: {
+            devShells.default = pkgs.mkShell {
+              nativeBuildInputs = with pkgs; [
+                wrapGAppsHook
+                perl
+                pkg-config
+              ];
+              buildInputs = with pkgs; [
+                openssl
+                glib
+                gtk3
+                gdk-pixbuf
+                webkitgtk.dev
+              ];
+              inputsFrom = [ inputs.holochain.devShells.${system}.holonix ];
+              packages = [ pkgs.nodejs-18_x pkgs.cargo-nextest ];
+
+              shellHook = ''
+                export GIO_MODULE_DIR=${pkgs.glib-networking}/lib/gio/modules/
+                export GIO_EXTRA_MODULES=${pkgs.glib-networking}/lib/gio/modules
+                export WEBKIT_DISABLE_COMPOSITING_MODE=1
+                unset CARGO_TARGET_DIR
+                unset CARGO_HOME
+              '';
+            };
+          };
+      };
+}
