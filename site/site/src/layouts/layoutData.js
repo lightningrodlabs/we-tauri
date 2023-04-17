8516acfdcd2257062ad105b28b06fe4d48c2651e
Guillem Córdoba <guillem.cordoba@gmail.com>
2023-04-17 14:03:41 +0200

    Initial site

---
+++ site/site/src/layouts/layoutData.js
@@ -0,0 +1,29 @@
+import { html } from "lit";
+
+import { PageTree } from "@rocket/engine";
+
+export const pageTree = new PageTree();
+await pageTree.restore(
+  new URL("../../pages/pageTreeData.rocketGenerated.json", import.meta.url)
+);
+
+export const layoutData = {
+  pageTree,
+  titleWrapperFn: (title) => title,
+  description: "Welcome to the Rocket Spark Landing Page example",
+  siteName: "We",
+
+  head__150: html`<link rel="stylesheet" href="resolve:#src/css/page.css" />`,
+
+  footer__10: html`
+    <rocket-content-area>
+      <rocket-columns>
+        <div>
+          <strong>Find Us</strong><br />
+          lightningrodlabs<br />
+          <a href="https://lightningrodlabs.org">https://lightningrodlabs.org>/a><br />
+        </div>
+      </rocket-columns>
+    </rocket-content-area>
+  `,
+};
