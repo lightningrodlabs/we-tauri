e3e5863627b53c67aee50865aaffd8c6f3f50b19
Wesley Finck <wesleyfinck@gmail.com>
2023-05-09 08:33:26 -0600

    drop current agent pub key from list of peers, to not send signal to self

--- dnas/sensemaker/zomes/coordinator/sensemaker/src/agent.rs
+++ dnas/sensemaker/zomes/coordinator/sensemaker/src/agent.rs
@@ -9,15 +9,21 @@ pub fn all_agents_typed_path() -> ExternResult<TypedPath> {
 
 #[hdk_extern]
 pub fn get_all_agents(_: ()) -> ExternResult<Vec<AgentPubKey>> {
+    let my_pub_key = agent_info()?.agent_initial_pubkey;
     let all_agents_path = all_agents_typed_path()?;
     let all_agents_links = get_links(
         all_agents_path.path_entry_hash()?, 
         LinkTypes::AllAgentsPath,
         None,
     )?;
-    let all_agents = all_agents_links
+    let all_agents: Vec<AgentPubKey> = all_agents_links
         .into_iter()
         .map(|link| EntryHash::from(link.target).into())
         .collect();
-    Ok(all_agents)
+    let all_other_agents = all_agents
+        .iter()
+        .filter(|agent| agent.clone() != &my_pub_key)
+        .cloned()
+        .collect();
+    Ok(all_other_agents)
 }
