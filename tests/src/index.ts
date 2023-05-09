db02084169ec9e551010fd3de6aa596c34f0eedd
Wesley Finck <wesleyfinck@gmail.com>
2023-05-09 08:22:17 -0600

    add agent related tests

--- tests/src/index.ts
+++ tests/src/index.ts
@@ -5,10 +5,12 @@ import sensemaker_config from './sensemaker_dna/sensemaker/sm_config';
 import applet_config from './sensemaker_dna/sensemaker/applet_config';
 import range from './sensemaker_dna/sensemaker/range';
 import dashboard from './sensemaker_dna/sensemaker/dashboard';
+import agent from './sensemaker_dna/sensemaker/agent';
 range();
 neighbourhood();
 applet_config();
 sensemaker_config();
 sensemaker_community_activator();
 dashboard();
+agent();
 
