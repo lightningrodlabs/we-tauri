f9f2800f3768607b2345a08a5e3f312e717d1d6d
Nicholas Stebbings <NICKOLARSE@RISEUP.NET>
2023-05-17 17:26:34 +0100

    Switch to vite (some cleanup needed)

---
+++ ui/app/vite.config.js
@@ -0,0 +1,59 @@
+import { defineConfig } from "vite";
+import { viteStaticCopy } from "vite-plugin-static-copy";
+
+const components = [
+  "dialog",
+  "dropdown",
+  "menu",
+  "menu-item",
+  "checkbox",
+  "divider",
+  "menu-label",
+  "option",
+  "select",
+  "tooltip",
+  "card",
+  "icon-button",
+  "button",
+  "icon",
+  "alert",
+  "input",
+  "spinner",
+  "avatar",
+  "skeleton",
+];
+const exclude = components.map(
+  (c) => `@shoelace-style/shoelace/dist/components/${c}/${c}.js`
+);
+export default defineConfig({
+  plugins: [
+    // checker({
+    //   typescript: true,
+    //   eslint: {
+    //     lintCommand: "eslint --ext .ts,.html src",
+    //   },
+    // }),
+    viteStaticCopy({
+      targets: [
+        {
+          src: "../../node_modules/@shoelace-style/shoelace/dist/assets",
+          dest: "shoelace",
+        },
+        {
+          src: "we_logo.png",
+          dest: "dist/assets",
+        },
+        {
+          src: "../applet-worker/dist/index.mjs",
+          dest: "",
+          rename: "applet-worker.js",
+        },
+        {
+          src: "../applet-iframe/dist/index.mjs",
+          dest: "",
+          rename: "applet-iframe.js",
+        },
+      ],
+    }),
+  ],
+});
