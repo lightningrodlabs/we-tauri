5f31c8832a1a9bb774bc831a1a5fe0c8f029bda2
Guillem Córdoba <guillem.cordoba@gmail.com>
2023-05-18 13:39:21 +0200

    Changed flake

--- flake.nix
+++ flake.nix
@@ -2,59 +2,94 @@
   description = "Template for Holochain app development";
 
   inputs = {
-    nixpkgs.follows = "holochain/nixpkgs";
+    versions.url  = "github:holochain/holochain?dir=versions/0_1";
 
-    holochain = {
-      url = "github:holochain/holochain";
-      inputs.versions.url = "github:holochain/holochain?dir=versions/0_1";
-    };
+    holochain-flake.url = "github:holochain/holochain";
+    holochain-flake.inputs.versions.follows = "versions";
+
+    nixpkgs.follows = "holochain-flake/nixpkgs";
+    flake-parts.follows = "holochain-flake/flake-parts";
   };
 
-  outputs = inputs @ { ... }:
-    inputs.holochain.inputs.flake-parts.lib.mkFlake
+  outputs = inputs:
+    inputs.flake-parts.lib.mkFlake
       {
         inherit inputs;
       }
       {
-        systems = builtins.attrNames inputs.holochain.devShells;
+        systems = builtins.attrNames inputs.holochain-flake.devShells;
         perSystem =
-          { config
+          { inputs'
+          , config
           , pkgs
           , system
+          , lib
+          , self'
           , ...
           }: {
             devShells.default = pkgs.mkShell {
-              nativeBuildInputs = with pkgs; [
-                wrapGAppsHook
+              inputsFrom = [ inputs'.holochain-flake.devShells.holonix ];
+              packages = [
+                pkgs.nodejs-18_x
+                # more packages go here
+                pkgs.cargo-nextest
+              ];
+              
+              buildInputs = (with pkgs; [
+                openssl
+
+                # this is required for glib-networking
+                glib
+              ])
+              ++ (lib.optionals pkgs.stdenv.isLinux
+                (with pkgs; [
+                  webkitgtk.dev
+                  gdk-pixbuf
+                  gtk3
+                  # Video/Audio data composition framework tools like "gst-inspect", "gst-launch" ...
+                  gst_all_1.gstreamer
+                  # Common plugins like "filesrc" to combine within e.g. gst-launch
+                  gst_all_1.gst-plugins-base
+                  # Specialized plugins separated by quality
+                  gst_all_1.gst-plugins-good
+                  gst_all_1.gst-plugins-bad
+                  gst_all_1.gst-plugins-ugly
+                  # Plugins to reuse ffmpeg to play almost every video format
+                  gst_all_1.gst-libav
+                  # Support the Video Audio (Hardware) Acceleration API
+                  gst_all_1.gst-vaapi
+                ]))
+              ++ lib.optionals pkgs.stdenv.isDarwin
+                (with self'.legacyPackages.apple_sdk'.frameworks; [
+                  AppKit
+                  CoreFoundation
+                  CoreServices
+                  Security
+                  IOKit
+                  WebKit
+                ])
+              ;
+
+              nativeBuildInputs = (with pkgs; [
                 perl
                 pkg-config
-              ];
-              buildInputs = with pkgs; [
-                openssl
-                glib
-                gtk3
-                gdk-pixbuf
-                webkitgtk.dev
-                # Video/Audio data composition framework tools like "gst-inspect", "gst-launch" ...
-                gst_all_1.gstreamer
-                # Common plugins like "filesrc" to combine within e.g. gst-launch
-                gst_all_1.gst-plugins-base
-                # Specialized plugins separated by quality
-                gst_all_1.gst-plugins-good
-                gst_all_1.gst-plugins-bad
-                gst_all_1.gst-plugins-ugly
-                # Plugins to reuse ffmpeg to play almost every video format
-                gst_all_1.gst-libav
-                # Support the Video Audio (Hardware) Acceleration API
-                gst_all_1.gst-vaapi
-              ];
-              inputsFrom = [ inputs.holochain.devShells.${system}.holonix ];
-              packages = [ pkgs.nodejs-18_x pkgs.cargo-nextest pkgs.cargo-tauri ];
+
+              ])
+              ++ (lib.optionals pkgs.stdenv.isLinux
+                (with pkgs; [
+                  wrapGAppsHook
+                ]))
+              ++ (lib.optionals pkgs.stdenv.isDarwin [
+                pkgs.xcbuild
+                pkgs.libiconv
+              ])
+              ;
 
               shellHook = ''
                 export GIO_MODULE_DIR=${pkgs.glib-networking}/lib/gio/modules/
                 export GIO_EXTRA_MODULES=${pkgs.glib-networking}/lib/gio/modules
                 export WEBKIT_DISABLE_COMPOSITING_MODE=1
+                unset CARGO_TARGET_DIR
               '';
             };
           };
