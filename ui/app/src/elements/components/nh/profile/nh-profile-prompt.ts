f837053b95292510ad8e0da4da564546c376872e
Nicholas Stebbings <nickolarse@riseup.net>
2023-06-16 12:37:10 +0100

    tweak profile prompt styles

--- ui/app/src/elements/components/nh/profile/nh-profile-prompt.ts
+++ ui/app/src/elements/components/nh/profile/nh-profile-prompt.ts
@@ -49,22 +49,23 @@ export class NHProfilePrompt extends NHComponentShoelace {
         display: flex;
       }
       #prompt {
-        --sl-color-primary-1000: var(--nh-theme-fg-on-dark);
         --sl-color-primary-500: var(--nh-theme-bg-subtle);
         
         --sl-color-primary-600: var(--nh-theme-bg-muted);
-        --sl-color-neutral-0: var(--nh-theme-bg-surface);
+        --sl-color-neutral-500: var(--nh-theme-bg-surface);
+        --sl-color-neutral-1000: var(--nh-theme-fg-on-dark);
+        --sl-color-primary-1000: var(--nh-theme-fg-on-dark);
         --sl-panel-background-color: var(--nh-theme-bg-surface);
 
         --sl-spacing-3x-small: calc(1px * var(--nh-spacing-md));
         --sl-input-height-medium:  calc(1rem * var(--nh-spacing-xs));
-        --sl-input-height-large:  1.5rem;
+        --sl-input-height-large:  4rem;
         --sl-input-border-width: 2px;
         --sl-input-color: var(--nh-theme-fg-on-dark);
         --sl-input-border-color: transparent;
         --sl-input-background-color: var(--nh-theme-bg-subtle);
-        --sl-input-font-size-medium: var(--nh-font-size-lg);
-        --sl-input-label-font-size-medium: var(--nh-font-size-xl);
+        --sl-input-font-size-medium: calc(1px * var(--nh-font-size-lg));
+        --sl-input-label-font-size-medium: calc(1px * var(--nh-font-size-xl));
 
         --sl-spacing-large: calc(1px * var(--nh-spacing-xl));
         --sl-border-radius-medium: calc(1px * var(--nh-radii-lg));
