f489d98ec706397d05c1dac4e93ca5f29c8bb058
Wesley Finck <wesleyfinck@gmail.com>
2023-07-12 15:40:57 -0700

    authorize zome calls for the lobby cell

--- ui/app/src/we-app.ts
+++ ui/app/src/we-app.ts
@@ -1,6 +1,6 @@
 import { ContextProvider } from "@lit-labs/context";
 import { state, query, customElement } from "lit/decorators.js";
-import { AppWebsocket, AdminWebsocket, InstalledCell } from "@holochain/client";
+import { AppWebsocket, AdminWebsocket, InstalledCell, ProvisionedCell } from "@holochain/client";
 import { ScopedElementsMixin } from "@open-wc/scoped-elements";
 import { LitElement, html, css } from "lit";
 
@@ -21,6 +21,8 @@ export class WeApp extends ScopedElementsMixin(LitElement) {
     const appWebsocket = await AppWebsocket.connect(`ws://localhost:9001`);
     console.log("Hello World!");
     const weAppInfo = await appWebsocket.appInfo( { installed_app_id: "we"} );
+    const cellId = (weAppInfo.cell_info["lobby"][0] as { provisioned: ProvisionedCell }).provisioned.cell_id;
+    await adminWebsocket.authorizeSigningCredentials(cellId);
     this._matrixStore = await MatrixStore.connect(appWebsocket, adminWebsocket, weAppInfo);
     new ContextProvider(this, matrixContext, this._matrixStore);
 
