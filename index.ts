826762401ce193b1f6c70592eea8a3db6755084c
Nicholas Stebbings <nickolarse@riseup.net>
2023-08-04 10:40:51 +0100

    exports fonts,  images

--- index.ts
+++ index.ts
@@ -1,11 +1,15 @@
+import { css, unsafeCSS } from 'lit';
 // @ts-ignore
 import DarkAsString from './build/dark/css/_variables.css';
-import { css, unsafeCSS } from 'lit';
+// @ts-ignore
+import b64fonts from './fonts.css';
+// @ts-ignore
+import b64images from './b64images.ts';
 // @ts-ignore
 import Dark from './build/dark/css/variables.css';
 
 const LitCSS = css`${unsafeCSS(DarkAsString)}`
 
 export {
-  Dark, LitCSS
+  Dark, b64images, b64fonts, LitCSS
 }
