5f47eca514392a6f7de0becbf9e4d5f1db3eae9d
Nicholas Stebbings <nickolarse@riseup.net>
2023-08-11 11:46:08 +0100

    add shoelace adapter

--- src/ancestors/base.ts
+++ src/ancestors/base.ts
@@ -1,15 +1,14 @@
 import { css, CSSResult, LitElement, unsafeCSS } from 'lit';
 
-import { Dark } from '@neighbourhoods/design-system-styles';
-// import shoelaceUIAdapter from '../styles/css/dark/shoelace-adapter.css?inline' assert { type: 'css' };
+import { Dark, LitCSS, Shoelace } from '@neighbourhoods/design-system-styles';
 // import materialUIAdapter from '../styles/css/dark/material-adapter.css?inline' assert { type: 'css' };
 import { ScopedRegistryHost } from '@lit-labs/scoped-registry-mixin';
 
 export class NHComponent extends ScopedRegistryHost(LitElement) {
   static styles: CSSResult|CSSResult[] = [css`${unsafeCSS(Dark)}`];
 }
-export class NHComponentShoelace extends ScopedRegistryHost(LitElement) {
-  static styles: CSSResult|CSSResult[] = [css`${unsafeCSS(Dark)}`];// , css`${unsafeCSS(shoelaceUIAdapter)}`];
+export class NHComponentShoelace extends NHComponent {
+  static styles: CSSResult|CSSResult[] = [css`${unsafeCSS(LitCSS)}`, css`${unsafeCSS(Shoelace)}`];
 }
 export class NHComponentMaterial extends ScopedRegistryHost(LitElement) {
   static styles: CSSResult|CSSResult[] = [css`${unsafeCSS(Dark)}`]//, css`${unsafeCSS(materialUIAdapter)}`];
