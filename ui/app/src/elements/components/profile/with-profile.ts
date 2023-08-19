7fc340d36315e506ec4d2249ee9f16c3de4285a6
Nicholas Stebbings <nickolarse@riseup.net>
2023-08-19 14:31:15 +0100

    prettify file and add identicon component

--- ui/app/src/elements/components/profile/with-profile.ts
+++ ui/app/src/elements/components/profile/with-profile.ts
@@ -1,13 +1,16 @@
-import { css, CSSResult, html, PropertyValueMap } from 'lit';
-import { customElement, property, query, state } from 'lit/decorators.js';
-import { NHButton, NHCard, NHComponent, NHComponentShoelace, NHProfileCard } from '@neighbourhoods/design-system-components';
+import { css, CSSResult, html } from 'lit';
+import { customElement, property, state } from 'lit/decorators.js';
+import {
+  NHComponent,
+  NHProfileCard,
+  NHProfileIdenticon,
+} from '@neighbourhoods/design-system-components';
 import { contextProvided } from '@lit-labs/context';
 import { deriveStore, get, StoreSubscriber } from '@holochain-open-dev/stores';
 import { MatrixStore } from '../../../matrix-store';
 import { matrixContext } from '../../../context';
-import { DnaHash, encodeHashToBase64 } from '@holochain/client';
+import { AgentPubKeyB64, DnaHash, encodeHashToBase64 } from '@holochain/client';
 import { NHProfilePrompt } from './nh-profile-prompt';
-import { Profile } from '@holochain-open-dev/profiles';
 
 @customElement('with-profile')
 export class WithProfile extends NHComponent {
@@ -16,41 +19,72 @@ export class WithProfile extends NHComponent {
   _matrixStore!: MatrixStore;
 
   @property()
-  weGroupId: DnaHash | undefined; 
+  weGroupId: DnaHash | undefined;
 
   @property()
-  component!: "card" | "prompt" | "identicon" ;
+  forAgentHash: AgentPubKeyB64 | undefined;
+
+  @property()
+  component!: 'card' | 'prompt' | 'identicon';
 
   @state()
   refreshed: boolean = false;
 
-  _profilesStore = new StoreSubscriber(this, () => this._matrixStore?.profilesStore(this.weGroupId as DnaHash));
+  _profilesStore = new StoreSubscriber(this, () =>
+    this._matrixStore?.profilesStore(this.weGroupId as DnaHash),
+  );
 
-  _selectedNeighbourhoodProfile = new StoreSubscriber(this, () => deriveStore(this._matrixStore.profilesStore(this.weGroupId as DnaHash), store => store!.myProfile), () => [this.refreshed]);
+  _selectedNeighbourhoodProfile = new StoreSubscriber(
+    this,
+    () =>
+      deriveStore(
+        this._matrixStore.profilesStore(this.weGroupId as DnaHash),
+        store => store!.myProfile,
+      ),
+    () => [this.refreshed],
+  );
 
   render() {
     this.refreshed = false;
     switch (true) {
-      case this.component == "card":
-        return this._selectedNeighbourhoodProfile.value.status != "complete" 
-          ? html`<slot><nh-profile-card
-          .loading=${true}
-          .agentHashB64=${encodeHashToBase64(this._matrixStore.myAgentPubKey)}>
-        </nh-profile-card>
-  </slot>` 
-          : html`<slot><nh-profile-card
-                        .agentAvatarSrc=${(this._selectedNeighbourhoodProfile.value as any).value?.fields?.avatar} 
-                        .agentName=${(this._selectedNeighbourhoodProfile.value as any).value?.nickname || "No Profile Created"} 
-                        .agentHashB64=${encodeHashToBase64(this._matrixStore.myAgentPubKey)}>
-                      </nh-profile-card>
-                </slot>`;
-      case this.component == "prompt":
+      case this.component == 'card':
+        return this._selectedNeighbourhoodProfile.value.status != 'complete'
+          ? html`<slot
+              ><nh-profile-card
+                .loading=${true}
+                .agentHashB64=${encodeHashToBase64(this._matrixStore.myAgentPubKey)}
+              >
+              </nh-profile-card>
+            </slot>`
+          : html`<slot
+              ><nh-profile-card
+                .agentAvatarSrc=${(this._selectedNeighbourhoodProfile.value as any).value?.fields
+                  ?.avatar}
+                .agentName=${(this._selectedNeighbourhoodProfile.value as any).value?.nickname ||
+                'No Profile Created'}
+                .agentHashB64=${encodeHashToBase64(this._matrixStore.myAgentPubKey)}
+              >
+              </nh-profile-card>
+            </slot>`;
+      case this.component == 'prompt':
         return (get((this._profilesStore.value as any).myProfile) as any).value
           ? html`<slot name="content"></slot>`
-          : html`<slot name="hero" slot="hero"></slot> <slot name="info" slot="info"></slot>
-                  <nh-profile-prompt .profilesStore=${this._profilesStore} @profile-created=${async () => { await this._profilesStore.value!.myProfile.reload(); this.refreshed = true;}}></nh-profile-prompt>`;
-        default:
-        break;
+          : html`<slot name="hero" slot="hero"></slot><slot name="info" slot="info"></slot>
+              <nh-profile-prompt
+                .profilesStore=${this._profilesStore}
+                @profile-created=${async () => {
+                  await this._profilesStore.value!.myProfile.reload();
+                  this.refreshed = true;
+                }}
+              ></nh-profile-prompt>`;
+      case this.component == 'identicon':
+        return html`<nh-profile-identicon
+          .agentAvatarSrc=${(this._selectedNeighbourhoodProfile.value as any).value?.fields?.avatar}
+          .agentName=${(this._selectedNeighbourhoodProfile.value as any).value?.nickname ||
+          'No Profile Created'}
+          .agentHashB64=${this.forAgentHash}
+          .profilesStore=${this._profilesStore}
+        ></nh-profile-identicon>`;
     }
   }
 
@@ -58,6 +92,7 @@ export class WithProfile extends NHComponent {
     return {
       'nh-profile-card': NHProfileCard,
       'nh-profile-prompt': NHProfilePrompt,
+      'nh-profile-identicon': NHProfileIdenticon,
     };
   }
 
@@ -71,6 +106,5 @@ export class WithProfile extends NHComponent {
         flex-direction: column;
       }
     `,
-  ]
-  
+  ];
 }
