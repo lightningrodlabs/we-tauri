6d6f4c68d005d56385df6e6a5c86ab8b59189e24
pospi <pospi@spadgos.com>
2023-09-15 14:15:36 +1000

    implement second RenderBlock test for recursive rendering

--- ui/app/src/elements/components/__tests__/render-block.test.ts
+++ ui/app/src/elements/components/__tests__/render-block.test.ts
@@ -12,26 +12,6 @@ import { RenderBlock } from '../render-block'
 
 customElements.define('render-block', RenderBlock)
 
-class Child1 extends ScopedRegistryHost(LitElement) {
-  static elementDefinitions = {
-    'nh-button': NHButton,
-  }
-
-  render() {
-    return html`<nh-button label="Child 1" />`
-  }
-}
-
-class Child2 extends ScopedRegistryHost(LitElement) {
-  static elementDefinitions = {
-    'nh-button': NHButton,
-  }
-
-  render() {
-    return html`<nh-button label="Child 2" />`
-  }
-}
-
 // :TODO: This should really be wrapped up into a helper generator fn in some
 //        shared @neighbourhoods/applet-framework module.
 const renderer = (tagName, classDef) => (i: HTMLElement, r: CustomElementRegistry) => {
@@ -40,27 +20,89 @@ const renderer = (tagName, classDef) => (i: HTMLElement, r: CustomElementRegistr
 }
 
 describe('RenderBlock', () => {
-  let harness
-
-  const initialRender = async () => {
-    // :TODO: unsure how to assign renderer from target components here
-    harness = await fixture(testHtml`<div>
-      <render-block .renderer=${renderer('child-1', Child1)}></render-block>
-      <render-block .renderer=${renderer('child-2', Child2)}></render-block>
-    </div>`)
+  const initialRender = async (theHTML) => {
+    return await fixture(theHTML)
   }
 
-  describe('Given two components sharing a nested child CustomElement ', () => {
-    beforeEach(async () => {
-      await initialRender()
-    })
+  describe('Given two components sharing a nested child CustomElement ', () =>
+  {
+    class Child1 extends ScopedRegistryHost(LitElement) {
+      static elementDefinitions = {
+        'nh-button': NHButton,
+      }
+
+      render() {
+        return html`<nh-button label="Child 1" />`
+      }
+    }
+
+    class Child2 extends ScopedRegistryHost(LitElement) {
+      static elementDefinitions = {
+        'nh-button': NHButton,
+      }
+
+      render() {
+        return html`<nh-button label="Child 2" />`
+      }
+    }
 
     test(`They should be able to render same child elements`, async () => {
+      const harness = await initialRender(testHtml`<div>
+        <render-block .renderer=${renderer('child-1', Child1)}></render-block>
+        <render-block .renderer=${renderer('child-2', Child2)}></render-block>
+      </div>`)
+
       const children = harness.querySelectorAll('render-block')
 
       expect(children.length).to.equal(2)
-      expect(children[0].shadowRoot.querySelector('child-1')).shadowDom.to.equal('<nh-button label="Child 1" />')
-      expect(children[1].shadowRoot.querySelector('child-2')).shadowDom.to.equal('<nh-button label="Child 2" />')
+      expect(children[0].shadowRoot?.querySelector('child-1')).shadowDom.to.equal('<nh-button label="Child 1" />')
+      expect(children[1].shadowRoot?.querySelector('child-2')).shadowDom.to.equal('<nh-button label="Child 2" />')
+    })
+  })
+
+  describe('Given a RenderBlock rendering a child CustomElement with dynamic children ', () =>
+  {
+    class Child3 extends ScopedRegistryHost(LitElement) {
+      static elementDefinitions = {
+        'nh-button': NHButton,
+      }
+
+      render() {
+        return html`<nh-button label="Child 3" />`
+      }
+    }
+
+    class DynamicRenderingChild extends ScopedRegistryHost(LitElement) {
+      static elementDefinitions = {
+        'render-block': RenderBlock,
+        'child-3': Child3,
+      }
+
+      render() {
+        return html`<div>
+          <render-block .renderer=${renderer('child-3', Child3)}></render-block>
+          <child-3></child-3>
+        </div>`
+      }
+    }
+
+    test(`RenderBlock children should be able to be rendered recursively no matter their parent elements`, async () => {
+      const harness = await initialRender(testHtml`<div>
+        <render-block .renderer=${renderer('dynamic-child', DynamicRenderingChild)}></render-block>
+        <render-block .renderer=${renderer('child-3', Child3)}></render-block>
+      </div>`)
+
+      const child = harness.querySelectorAll('render-block')[0]
+      expect(child).shadowDom.to.equal('<div><dynamic-child /></div>', { ignoreAttributes: [{ tags: ['div'], attributes: ['style'] }] })
+
+      const nestedRoot = child?.shadowRoot?.querySelector('dynamic-child')
+      expect(nestedRoot).shadowDom.to.equal('<div><render-block></render-block><child-3></child-3></div>')
+      expect(nestedRoot?.shadowRoot?.querySelector('child-3')).shadowDom.to.equal('<nh-button label="Child 3" />')
+
+      const nestedChild = nestedRoot?.shadowRoot?.querySelector('render-block')
+      expect(nestedChild).shadowDom.to.equal('<div><child-3 /></div>', { ignoreAttributes: [{ tags: ['div'], attributes: ['style'] }] })
+
+      expect(nestedChild?.shadowRoot?.querySelector('child-3')).shadowDom.to.equal('<nh-button label="Child 3" />')
     })
   })
 
