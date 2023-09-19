14489856b6f12869a288f809f4ebd9cf42a29b6d
Wesley Finck <wesleyfinck@gmail.com>
2023-09-18 17:14:43 -0700

    remove lit labs scoped element mixin from render block and instantiate a scoped registry per instance rather than class

--- ui/app/src/elements/components/render-block.ts
+++ ui/app/src/elements/components/render-block.ts
@@ -1,19 +1,30 @@
 import { ref } from "lit/directives/ref.js";
-import { ScopedRegistryHost as ScopedElementsMixin } from "@lit-labs/scoped-registry-mixin"
 import { css, html, LitElement } from "lit";
 import { property } from "lit/decorators.js";
 import { Renderer } from "@neighbourhoods/nh-launcher-applet";
 
-export class RenderBlock extends ScopedElementsMixin(LitElement) {
+export class RenderBlock extends LitElement {
   @property()
   renderer!: Renderer;
 
+  registry?: CustomElementRegistry;
+
   renderRenderer(element: Element | undefined) {
     if (element) {
-      this.renderer(element as HTMLElement, this.constructor.registry || customElements);
+      this.renderer(element as HTMLElement, this.registry || customElements);
     }
   }
 
+  override createRenderRoot() {
+    this.registry = new CustomElementRegistry()
+
+    const renderRoot = (this.renderOptions.creationScope = this.attachShadow({
+      mode: 'open',
+      customElements: this.registry,
+    }));
+
+    return renderRoot;
+  }
   render() {
     return html`<div
       style="display: contents"
@@ -28,7 +39,4 @@ export class RenderBlock extends ScopedElementsMixin(LitElement) {
       }
     `,
   ];
-
-  // :IMPORTANT: must be defined or the component's locally scoped CustomElementRegistry will not be inited
-  static elementDefinitions = {}
 }
