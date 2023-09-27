76746eb3830ba65b5c9f2bf6012a7b4a438e33a9
Nick Stebb <nickolarse@riseup.net>
2023-09-27 18:13:19 +1300

    move data uri validation helper

--- ui/app/src/elements/components/helpers/functions.ts
+++ ui/app/src/elements/components/helpers/functions.ts
@@ -1,5 +1,9 @@
 import { html } from "lit";
 
+export function isDataURL(s) {
+  return !!s.match(isDataURL.regex);
+}
+isDataURL.regex = /^\s*data:([a-z]+\/[a-z0-9\-\+]+(;[a-z\-]+\=[a-z0-9\-]+)?)?(;base64)?,[a-z0-9\!\$\&\'\,\(\)\*\+\,\;\=\-\.\_\~\:\@\/\?\%\s]*\s*$/i;
 
 export const cleanForUI = (propertyName: string) =>
   propertyName.split('_').map(capitalize).join(' ');
