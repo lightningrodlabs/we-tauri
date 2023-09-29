792014845b1c29851182e71fa6b5a8ee99248a96
Nicholas Stebbings <nickolarse@riseup.net>
2023-09-29 18:54:39 +1300

    add better typing

--- ui/app/src/elements/components/applet-list-item.ts
+++ ui/app/src/elements/components/applet-list-item.ts
@@ -1,113 +1,112 @@
 import { NHComponent, NHButton, NHPageHeaderCard } from "@neighbourhoods/design-system-components";
-import { b64images } from "@neighbourhoods/design-system-styles";
 import { html, css } from "lit";
 import { property } from "lit/decorators.js";
 import { InstallableApplets } from "./installable-applets";
 import { contextProvided } from "@lit-labs/context";
-import { MatrixStore } from "../../matrix-store";
+import { AppletInstanceInfo, MatrixStore } from "../../matrix-store";
 import { matrixContext } from "../../context";
 import { AppInfo } from "@holochain/client";
 import { Snackbar } from "@scoped-elements/material-web";
+import { SlTooltip } from "@scoped-elements/shoelace";
 
 export class AppletListItem extends NHComponent {
   @contextProvided({ context: matrixContext, subscribe: true })
   matrixStore!: MatrixStore;
   
   @property()
+  appletStatus!: string;
+  
+  @property()
+  appletInfo!: AppletInstanceInfo;
+  
+  @property()
   onConfigureWidgets!: () => void;
-
-  @property()
-  appletStatus;
-
-  @property()
-  appletInfo;
 
   @property()
   onDelete!: () => void;
 
   async disableApp(appInfo: AppInfo) {
+    const list = (this.parentNode as any);
     this.matrixStore.disableApp(appInfo)
       .then(() => {
-        (this.shadowRoot?.getElementById("app-disabled-snackbar") as Snackbar).show();
-        this.requestUpdate();
+        (list.getElementById("app-disabled-snackbar") as Snackbar).show();
+        list.host.refresh();
       }).catch((e) => {
         console.log("Error: ", e);
-        (this.shadowRoot?.getElementById("error-snackbar") as Snackbar).show();
+        (list.getElementById("error-snackbar") as Snackbar).show();
       });
   }
 
   async enableApp(appInfo: AppInfo) {
+    const list = (this.parentNode as any);
     this.matrixStore.enableApp(appInfo)
       .then(() => {
-        (this.shadowRoot?.getElementById("app-enabled-snackbar") as Snackbar).show();
-        this.requestUpdate();
+        (list.getElementById("app-enabled-snackbar") as Snackbar).show();
+        list.host.refresh();
       }).catch((e) => {
         console.log("Error: ", e);
-        (this.shadowRoot?.getElementById("error-snackbar") as Snackbar).show();
+        (list.getElementById("error-snackbar") as Snackbar).show();
       });
   }
 
   render() {
     return html`<div class="container">
-                      <mwc-card style="margin: 5px;">
-                        <div
-                          class="row"
-                          style="background: #645d69; color: white; align-items: center; padding: 5px; padding-left: 15px; font-size: 1.2em"
-                        >
-                          <img
-                              style="margin-right: 10px;"
-                              class="applet-image"
-                              src=${this.appletInfo.applet.logoSrc!}
-                            />
-                          <strong>${this.appletInfo.applet.customName}</strong>
-                          <div class="row" style="margin-left: auto; align-items: center;">
-                            <span style="color: gray; margin-right: 25px;">${this.appletStatus}</span>
-      
-                            <sl-tooltip placement="top" content="configure widgets" hoist>
-                              <mwc-button
-                                icon="share"
-                                style="margin-right: 10px;"
-                                @click=${() => {this.onConfigureWidgets()}}
-                                label="configure widgets"
-                              >
-                              </mwc-button>
-                            </sl-tooltip>
-      
-                            ${this.appletStatus === "RUNNING"
-                              ? html`
-                                <nh-button
-                                  class="disable-button"
-                                  label="DISABLE"
-                                  .iconImageB64=${b64images.icons.plus}
-                                  @click=${async () => await this.disableApp(this.appletInfo.appInfo)}
-                                ></nh-button>
-                                `
-                              : html`
-                                <nh-button
-                                  class="start-button"
-                                  label="START"
-                                  .iconImageB64=${b64images.icons.plus}
-                                  @click=${async () => await this.enableApp(this.appletInfo.appInfo)}
-                                ></nh-button>
-                                `
-                            }
-                            <nh-button
-                              class="delete-button"
-                              label="UNINSTALL"
-                              .iconImageB64=${b64images.icons.plus}
-                              @click=${() => { this.onDelete() }
-                              }
-                            >
-                            </nh-button>
-                          </div>
-                        </div>
-                      </mwc-card>
+                  <div style="display: flex; flex: 1; align-items: center; gap: calc(1px * var(--nh-spacing-sm));">
+                    <img
+                      class="applet-image"
+                      src=${this.appletInfo.applet.logoSrc!}
+                    />
+                    <h4>${this.appletInfo.applet.customName}</h4>
+                  </div>
+                  <div style="display: flex; flex: 3; align-items: center; gap: calc(1px * var(--nh-spacing-lg)); justify-content: flex-end;">
+                    <h4>${this.appletStatus}</h4>
+
+                    <sl-tooltip placement="top" content="Configure Widgets" hoist>
+                      <nh-button
+                        .clickHandler=${() => {this.onConfigureWidgets()}}
+                        label="Configure"
+                        .variant=${"neutral"}
+                        .size=${"md"}
+                      >
+                      </nh-button>
+                    </sl-tooltip>
+
+                    ${this.appletStatus === "RUNNING"
+                      ? html`
+                        <nh-button
+                          class="disable-button"
+                          label="Disable"
+                          .variant=${"warning"}
+                          .size=${"md"}
+                          .clickHandler=${async () => await this.disableApp(this.appletInfo.appInfo)}
+                        ></nh-button>
+                        `
+                      : html`
+                        <nh-button
+                          class="start-button"
+                          label="Start" 
+                          .variant=${"success"}
+                          .size=${"md"}
+                          .clickHandler=${async () => await this.enableApp(this.appletInfo.appInfo)}
+                        ></nh-button>
+                      `
+                    }
+                    <nh-button
+                      class="delete-button"
+                      label="Uninstall"
+                      .variant=${"danger"}
+                      .size=${"md"}
+                      .clickHandler=${() => { this.onDelete() }}
+                    >
+                    </nh-button>
+                  </div>
                 </div> 
     `;
   }
 
   static elementDefinitions = {
       "nh-button": NHButton,
+      "sl-tooltip": SlTooltip,
       "nh-page-header-card": NHPageHeaderCard,
       "installable-applets": InstallableApplets,
   }
@@ -116,9 +115,18 @@ export class AppletListItem extends NHComponent {
     return [
       css`
         .container {
-          display:grid;
-          grid-template-rows: auto 1fr;
-          width: 100%;
+          display: flex;
+          gap: calc(1px * var(--nh-spacing-lg));
+          justify-content: space-between;
+        }
+
+        img {
+          height: 2rem;
+          object-fit: cover;
+        }
+
+        h4 {
+          color: var(--nh-theme-fg-muted); 
         }
     `];
   }
