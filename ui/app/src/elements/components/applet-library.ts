620dbb825da7a039d2e926decdd3d1e192a5764f
Nicholas Stebbings <nickolarse@riseup.net>
2023-09-29 15:10:46 +1300

    move applet library to separate component

---
+++ ui/app/src/elements/components/applet-library.ts
@@ -0,0 +1,60 @@
+import { css, html } from "lit";
+import { property, query } from "lit/decorators.js";
+
+import { NHButton, NHComponent, NHPageHeaderCard } from "@neighbourhoods/design-system-components";
+import { b64images } from "@neighbourhoods/design-system-styles";
+import { InstallableApplets } from "./installable-applets";
+import { InstallFromFsDialog } from "../dialogs/install-from-file-system";
+
+export class AppletLibrary extends NHComponent {
+  @property()
+  toggleVisible!: () => void;
+
+  @query("#install-from-fs-dialog")
+  _installFromFsDialog!: InstallFromFsDialog;
+
+  render() {
+    return html`
+    <div class="container">
+      <nh-page-header-card
+        .heading=${"Applet Library"}
+      >
+        <img
+          src="data:image/svg+xml;base64,${b64images.icons.backCaret}"
+          slot="secondary-action"
+          @click=${this.toggleVisible}
+        />
+        <nh-button
+          .label=${"Upload Applet File"}
+          .variant=${"primary"}
+          .size=${"md"}
+          .clickHandler=${() => {this._installFromFsDialog?.open()}}
+          slot="primary-action"
+        ></nh-button>
+      </nh-page-header-card>
+
+      <installable-applets></installable-applets>
+
+      <install-from-fs-dialog id="install-from-fs-dialog"></install-from-fs-dialog>
+    </div>
+    `;
+  }
+
+  static elementDefinitions = {
+      "nh-button": NHButton,
+      "nh-page-header-card": NHPageHeaderCard,
+      "installable-applets": InstallableApplets,
+      "install-from-fs-dialog": InstallFromFsDialog,
+  }
+
+  static get styles() {
+    return [
+      css`
+        .container {
+          display:grid;
+          grid-template-rows: auto 1fr;
+          width: 100%;
+        }
+    `];
+  }
+}
