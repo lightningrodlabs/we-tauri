b1cafedaa8c2b3c3b72f35612c3a9cd6be64d98e
Nicholas Stebbings <nickolarse@riseup.net>
2023-10-06 09:59:06 +1300

    fix uninstall dialog

--- ui/app/src/elements/dialogs/uninstall-applet.ts
+++ ui/app/src/elements/dialogs/uninstall-applet.ts
@@ -1,14 +1,16 @@
 import { CSSResult, html } from "lit";
-import { property } from "lit/decorators.js";
+import { property, query } from "lit/decorators.js";
 
 import { NHButton, NHCard, NHComponentShoelace, NHDialog } from "@neighbourhoods/design-system-components";
 
 export class UninstallApplet extends NHComponentShoelace {
   @property()
   private _isOpen: boolean = false;
+  @query('nh-dialog')
+  private _dialog;
 
   open() {
-    this._isOpen = true;
+    this._dialog.showDialog();
   }
 
   confirm() {
@@ -27,7 +29,7 @@ export class UninstallApplet extends NHComponentShoelace {
                   .title=${"Uninstall Applet"}
                   .alertMessage=${"Are you sure you want to uninstall this applet?"}
                   .alertType=${"warning"}
-                  .dialogType=${"applet-install"}
+                  .dialogType=${"applet-uninstall"}
                   .size=${"medium"}
                   .handleOk=${this.confirm}
                   .isOpen=${this._isOpen}
@@ -35,15 +37,14 @@ export class UninstallApplet extends NHComponentShoelace {
                 >
                 <div slot="inner-content">
                   <nh-card .theme=${"dark"} .title="" .heading="" class="nested-card">
-                  <ul>
-                  <li>This will <strong>delete all data</strong> you have stored within this applet.</li>
-                  <li>In case this applet has been federated with other neighbourhoods, <b>it will also be removed from those neighbourhoods</b>.</li>
-                  </ul>
-                  <p>
-                    Other members of the neighbourhood (or neighbourhoods this applet is federated with) will still be able to use their own instance of the applet.
+                    <ul>
+                      <li>This will <strong>delete all data</strong> you have stored within this applet.</li>
+                      <li>In case this applet has been federated with other neighbourhoods, <b>it will also be removed from those neighbourhoods</b>.</li>
+                    </ul>
+                    <p slot="footer">
+                      Other members of the neighbourhood (or neighbourhoods this applet is federated with) will still be able to use their own instance of the applet.
                     </p>
-                    <div slot="footer"></div>
-                  <nh-card>
+                  </nh-card>
                   </div>
                 </nh-dialog>
     `;
