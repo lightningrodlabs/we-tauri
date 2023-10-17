464b1f70d7273652e8504c50ba169e2bd439d8f0
Nicholas Stebbings <nickolarse@riseup.net>
2023-10-18 10:55:09 +1300

    add dimension list, make a layout component

--- ui/app/src/nh-config/nh-global-config.ts
+++ ui/app/src/nh-config/nh-global-config.ts
@@ -8,18 +8,15 @@ import { DnaHash } from "@holochain/client";
 import { NHButton, NHComponent } from "@neighbourhoods/design-system-components";
 import CreateMethod from "./create-method-form";
 import CreateDimension from "./create-dimension-form";
+import DimensionList from "./dimension-list";
 
 export default class NHGlobalConfig extends NHComponent {
-  @contextProvided({ context: matrixContext, subscribe: true })
-  _matrixStore!: MatrixStore;
-
-  @contextProvided({ context: weGroupContext, subscribe: true })
-  weGroupId!: DnaHash;
 
   render() {
     return html`
       <create-dimension></create-dimension>
       <create-method></create-method>
+      <dimension-list></dimension-list>
     `;
   }
 
@@ -28,6 +25,7 @@ export default class NHGlobalConfig extends NHComponent {
     "nh-button": NHButton,
     "create-dimension": CreateDimension,
     "create-method": CreateMethod,
+    "dimension-list": DimensionList
   }
 
   static get styles() {
@@ -37,6 +35,12 @@ export default class NHGlobalConfig extends NHComponent {
         flex: 1;
         place-content: center;
         color: var(--nh-theme-fg-default); 
+        grid-template-columns: 2fr 1fr;
+        grid-template-rows: 1fr;
+      }
+
+      dimension-list {
+        grid-column: 1/-1;
       }
     `;
   }
