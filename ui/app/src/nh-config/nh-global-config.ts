8a096b8463538537bae39b4c87827776ce39baac
Nicholas Stebbings <nickolarse@riseup.net>
2023-10-26 11:50:17 +1300

    handle dimension created event

--- ui/app/src/nh-config/nh-global-config.ts
+++ ui/app/src/nh-config/nh-global-config.ts
@@ -1,15 +1,16 @@
-import { html, css } from "lit";
-import { contextProvided } from "@lit-labs/context";
-import { StoreSubscriber } from "lit-svelte-stores";
+import { html, css } from 'lit';
+import { contextProvided } from '@lit-labs/context';
+import { StoreSubscriber } from 'lit-svelte-stores';
 
-import { MatrixStore } from "../matrix-store";
-import { matrixContext, weGroupContext } from "../context";
-import { DnaHash } from "@holochain/client";
+import { MatrixStore } from '../matrix-store';
+import { matrixContext, weGroupContext } from '../context';
+import { DnaHash } from '@holochain/client';
 
-import { NHButton, NHComponent } from "@neighbourhoods/design-system-components";
-import CreateMethod from "./create-method-form";
-import CreateDimension from "./create-dimension-form";
-import DimensionList from "./dimension-list";
+import { NHButton, NHComponent } from '@neighbourhoods/design-system-components';
+import CreateMethod from './create-method-form';
+import CreateDimension from './create-dimension-form';
+import DimensionList from './dimension-list';
+import { query } from 'lit/decorators.js';
 
 export default class NHGlobalConfig extends NHComponent {
   @contextProvided({ context: matrixContext, subscribe: true })
@@ -18,31 +19,45 @@ export default class NHGlobalConfig extends NHComponent {
   @contextProvided({ context: weGroupContext, subscribe: true })
   weGroupId!: DnaHash;
 
-  _sensemakerStore = new StoreSubscriber(this, () => this._matrixStore?.sensemakerStore(this.weGroupId));
+  @query('dimension-list')
+  _list;
+  @query('create-dimension')
+  _dimensionForm;
+
+  _sensemakerStore = new StoreSubscriber(this, () =>
+    this._matrixStore?.sensemakerStore(this.weGroupId),
+  );
 
   render() {
     return html`
-      <create-dimension .sensemakerStore=${this._sensemakerStore.value}></create-dimension>
-      <dimension-list .sensemakerStore=${this._sensemakerStore.value}></dimension-list>
-      <create-method></create-method>
+      <main
+        @dimension-created=${async (_: CustomEvent) => {
+          await this._dimensionForm.resetForm(); 
+          await this._dimensionForm.requestUpdate();
+          await this._list.fetchDimensionEntries()}
+        }
+      >
+        <create-dimension .sensemakerStore=${this._sensemakerStore.value}></create-dimension>
+        <dimension-list .sensemakerStore=${this._sensemakerStore.value}></dimension-list>
+        <create-method></create-method>
+      </main>
     `;
   }
 
-
   static elementDefinitions = {
-    "nh-button": NHButton,
-    "create-dimension": CreateDimension,
-    "create-method": CreateMethod,
-    "dimension-list": DimensionList
-  }
+    'nh-button': NHButton,
+    'create-dimension': CreateDimension,
+    'create-method': CreateMethod,
+    'dimension-list': DimensionList,
+  };
 
   static get styles() {
     return css`
-      :host {
+      main {
         display: grid;
         flex: 1;
-        place-content: center;
-        color: var(--nh-theme-fg-default); 
+        place-content: start;
+        color: var(--nh-theme-fg-default);
         grid-template-columns: 2fr 1fr;
         grid-template-rows: 1fr 1fr;
         padding: calc(1px * var(--nh-spacing-xl));
