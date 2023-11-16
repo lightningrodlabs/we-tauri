40d613de2857635ac49ddf44d311dee67ca209f9
pospi <pospi@spadgos.com>
2023-11-16 14:11:54 +1100

    Revert "update widget config save/load methods to handle as singular entry ref", need to manage multiple config blocks
    This reverts commit 66c0b07899dd92d3df0be60e3485b88d5c914d3b.

--- dnas/sensemaker/zomes/coordinator/widgets/src/lib.rs
+++ dnas/sensemaker/zomes/coordinator/widgets/src/lib.rs
@@ -10,45 +10,41 @@ struct QueryParams {
 }
 
 #[hdk_extern]
-fn get_assessment_widget_tray_config(QueryParams { resource_def_eh }: QueryParams) -> ExternResult<AssessmentWidgetBlockConfig> {
+fn get_assessment_widget_tray_config(QueryParams { resource_def_eh }: QueryParams) -> ExternResult<Vec<DimensionBinding>> {
     let links = get_links(
         resource_def_eh,
         LinkTypes::WidgetConfigs,
         None,
     )?;
 
-    let link = links.first()
-        .ok_or(wasm_error!(WasmErrorInner::Guest(String::from(
-            "unable to locate widget configuration for Resource Def"
-        ))))?;
+    Ok(links.iter()
+        .filter_map(|link| {
+            let maybe_record = get(
+                EntryHash::from(link.clone().target),
+                GetOptions::default(),
+            );
 
-    let maybe_record = get(
-        EntryHash::from(link.clone().target),
-        GetOptions::default(),
-    );
-
-    match maybe_record {
-        Err(e) => Err(WasmError::from(e)),
-        Ok(None) => Err(wasm_error!(WasmErrorInner::Guest(String::from(
-                "unable to load widget configuration for Resource Def"
-            )))),
-        Ok(Some(record)) =>
-            entry_from_record::<AssessmentWidgetBlockConfig>(record)
-                .map_or(Err(wasm_error!(WasmErrorInner::Guest(String::from(
-                    "unable to decode widget configuration for Resource Def"
-                )))), |f| Ok(f))
-    }
+            match maybe_record {
+                Err(_) => None, // :TODO: error handling
+                Ok(None) => None,
+                Ok(Some(record)) =>
+                    entry_from_record::<DimensionBinding>(record)
+                        .map_or(None, |f| Some(f))
+            }
+        })
+        .collect()
+    )
 }
 
 #[derive(Serialize, Deserialize, Debug, SerializedBytes, Clone)]
 #[serde(rename_all = "camelCase")]
 struct UpdateParams {
     pub resource_def_eh: EntryHash,
-    pub widget_config: AssessmentWidgetBlockConfig,
+    pub widget_configs: Vec<DimensionBinding>,
 }
 
 #[hdk_extern]
-fn set_assessment_widget_tray_config(UpdateParams { resource_def_eh, widget_config }: UpdateParams) -> ExternResult<EntryHash> {
+fn set_assessment_widget_tray_config(UpdateParams { resource_def_eh, widget_configs }: UpdateParams) -> ExternResult<bool> {
     // check existing configuration links
     let existing_links = get_links(
         resource_def_eh.to_owned(),
@@ -59,37 +55,41 @@ fn set_assessment_widget_tray_config(UpdateParams { resource_def_eh, widget_conf
         .map(|l| l.target.to_owned().into_entry_hash())
         .collect();
 
-    // process newly passed config
-    let config_hash = hash_entry(widget_config.to_owned()).ok();
+    // process all passed configs
+    let widget_config_hashes: Vec<EntryHash> = widget_configs.iter()
+        .filter_map(|c| {
+            let config_hash = hash_entry(c).ok();
 
-    // ignore previously saved values
-    if config_hash == None || link_targets.contains(&config_hash) {
-        // return hash of stored widget configuration data
-        return config_hash.ok_or(wasm_error!(WasmErrorInner::Guest(String::from(
-            "unable to determine widget config entry hash"
-        ))))
-    }
+            // ignore previously saved values
+            if config_hash == None || link_targets.contains(&config_hash) {
+                // return hash for comparing when removing stale configs
+                return config_hash.to_owned()
+            }
 
-    // store new config block and link to Resource Def
-    // :TODO: error handling
-    create_entry(&EntryTypes::AssessmentWidgetBlockConfig(widget_config))?;
-    create_link(
-        resource_def_eh.to_owned(),
-        config_hash.clone().unwrap(),
-        LinkTypes::WidgetConfigs,
-        (),
-    )?;
+            // store new config blocks and link to Resource Def
+            // :TODO: error handling
+            create_entry(&EntryTypes::DimensionBinding(c.clone()));
+            create_link(
+                resource_def_eh.to_owned(),
+                config_hash.clone().unwrap(),
+                LinkTypes::WidgetConfigs,
+                (),
+            );
+
+            config_hash
+        })
+        .collect();
 
     // unlink any existing configs which were not found in the input
     // :TODO: error handling
     existing_links.iter()
         .for_each(|l| {
-            if l.target.to_owned().into_entry_hash() == config_hash {
+            if widget_config_hashes.contains(&l.target.clone().into_entry_hash().unwrap()) {
                 return
             }
 
             delete_link(l.create_link_hash.clone());
         });
 
-    Ok(config_hash.unwrap())
+    Ok(true)
 }
