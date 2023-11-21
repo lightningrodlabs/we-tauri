b2a223336389910f2f1af28d79fcf3564237419c
pospi <pospi@spadgos.com>
2023-11-21 18:25:18 +1100

    update Nix env to HC0.2.x release series

--- flake.nix
+++ flake.nix
@@ -2,10 +2,11 @@
   description = "Template for Holochain app development";
 
   inputs = {
-    versions.url  = "github:holochain/holochain?dir=versions/0_1";
+    versions.url  = "github:holochain/holochain?dir=versions/0_2";
 
     holochain-flake.url = "github:holochain/holochain";
     holochain-flake.inputs.versions.follows = "versions";
+    holochain-flake.inputs.holochain.url = "github:holochain/holochain/holochain-0.2.2";
 
     nixpkgs.follows = "holochain-flake/nixpkgs";
     flake-parts.follows = "holochain-flake/flake-parts";
@@ -27,10 +28,10 @@
           }: {
             devShells.default = pkgs.mkShell {
               inputsFrom = [ inputs'.holochain-flake.devShells.holonix ];
-              packages = [
+              packages = with pkgs; [
                 pkgs.nodejs-18_x
-                # more packages go here
-              ];             
+                nodePackages.pnpm
+              ];
             };
           };
       };
