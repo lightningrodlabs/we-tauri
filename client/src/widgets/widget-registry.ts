35f18deb082c017953aa5cbeb21e345f00a37957
Nicholas Stebbings <nickolarse@riseup.net>
2023-11-22 09:53:43 +1300

    add widget registration interface/types

--- client/src/widgets/widget-registry.ts
+++ client/src/widgets/widget-registry.ts
@@ -1,3 +1,38 @@
-export interface AssessmentWidgetRegistration {
-  
+import { Entry, EntryHash } from "@holochain/client";
+import { IAssessDimensionWidget, IDisplayDimensionWidget } from ".";
+
+export interface AssessmentWidgetRegistrationInput {
+  applet_eh: EntryHash, // Applet entry hash
+  widget_key: string,  // keyof an AssessmentWidgetConfigDict
+  name: string,
+  range_eh: EntryHash, // The EntryHash for the created Range object (should probably just be the range object when returned from the API)
+  kind: AssessmentWidgetKind
 }
+export type AssessmentWidgetRegistration = AssessmentWidgetRegistrationInput & {
+  assessment_registration_eh: EntryHash
+}
+
+type AssessmentWidgetConfigDict = Record<string, AssessmentWidgetConfig>
+// e.g.
+// {
+//   "importance": { // widget_key
+//     name: "Importance Ranker",
+//     range: {
+//       min: 0,
+//       max: 10
+//     },
+//     component: ImportanceWidget,
+//     kind: "input"
+//   }
+// }
+
+type AssessmentWidgetConfig = {
+  name: string,
+  range: Range,
+  component: AssessmentWidget,
+  kind: AssessmentWidgetKind 
+}
+
+type AssessmentWidget = IAssessDimensionWidget | IDisplayDimensionWidget;
+
+type AssessmentWidgetKind = "input" | "output";
