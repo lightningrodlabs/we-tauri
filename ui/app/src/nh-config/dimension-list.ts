5f422a6040f4a32bd16298e0d4eba772228d9161
Nicholas Stebbings <nickolarse@riseup.net>
2023-11-23 15:34:54 +1300

    remove erroneous code

--- ui/app/src/nh-config/dimension-list.ts
+++ ui/app/src/nh-config/dimension-list.ts
@@ -32,9 +32,6 @@ export default class DimensionList extends NHComponent {
   sensemakerStore!: SensemakerStore;
 
   @property()
-  dimensionSelected: boolean = false;
-
-  @property()
   dimensionType: "input" | "output" = "input";
 
   @state()
@@ -45,18 +42,6 @@ export default class DimensionList extends NHComponent {
 
   @state()
   private _rangeEntries!: Array<Range & { range_eh: EntryHash }>;
-  
-  @state()
-  _selectedInputDimensionIndex!: number;
-
-  @state()
-  methodInputDimensions: Array<Dimension & { methodEh: EntryHashB64, dimensionEh?: EntryHashB64 }> = [];
-
-  @state()
-  filteredMethodInputDimensions: Array<Dimension & { methodEh?: EntryHashB64, dimensionEh?: EntryHashB64 }> = [];
-
-  @state()
-  private _methodMapping = new StoreSubscriber(this, () => this.sensemakerStore.methodDimensionMapping());
 
   async fetchDimension(entryHash: EntryHash) : Promise<CallZomeResponse> {
     try {
@@ -140,36 +125,9 @@ export default class DimensionList extends NHComponent {
   async firstUpdated(_changedProperties: PropertyValueMap<any> | Map<PropertyKey, unknown>) {
     await this.fetchDimensionEntries()
     await this.fetchRangeEntries()
-    // NOTE: The following retrieves a mapping for method entry hashes to their respective input/output dimension hashes, but ONLY for those registered in an appletConfig.
-    if(typeof this._methodMapping.value == 'undefined') return;
-    
-    // If there is a method mapping (from applet config) it will be included here
-    const dimensions : any = [];
-    for (const [methodEh,{inputDimensionEh}] of Object.entries(this._methodMapping.value)) {
-      const response = await this.fetchDimension(inputDimensionEh);
-      try {
-        const dimension = decode(response.entry.Present.entry) as any;
-        dimension.methodEh = methodEh;
-        dimensions.push(dimension)
-      } catch (error) {
-        console.log('Error decoding dimension payload: ', error);
-      }
-    }
-    this.methodInputDimensions = [...this.methodInputDimensions, ...this._dimensionEntries, ...dimensions];
-
-    // Select the first item in the list:
-    // this.dispatchEvent(new CustomEvent("input-dimension-selected", {
-    //   detail: { range: this._rangeEntries[this._rangeEntries.length-1], dimensionEh: this._dimensionEntries[this._dimensionEntries.length-1].dimension_eh },
-    //   bubbles: true,
-    //   composed: true,
-    // }));
-    this.dimensionSelected = true;
   }
 
   protected updated(_changedProperties: PropertyValueMap<any> | Map<PropertyKey, unknown>): void {
-    if(_changedProperties.has('methodInputDimensions') && !!_changedProperties.get('methodInputDimensions')) {
-      this.filteredMethodInputDimensions = this.methodInputDimensions.filter(dimension => dimension?.methodEh);
-    }
 
     if(_changedProperties.has('_dimensionEntries') || _changedProperties.has('_rangeEntries')) {
       if(typeof this._rangeEntries == 'undefined') return;
@@ -177,13 +135,20 @@ export default class DimensionList extends NHComponent {
       try {
         const tableRecords = this._dimensionEntries.filter((dimension: Dimension) => this.dimensionType == 'input' ? !dimension.computed : dimension.computed)
           .reverse()
-          .map((dimension: Dimension & { dimension_eh: EntryHash; }, dimensionIndex: number) => {
+          .map((dimension: Dimension & { dimension_eh: EntryHash; }) => {
             const range = this._rangeEntries
-            .find((range: Range & { range_eh: EntryHash; }) =>
-              encodeHashToBase64(range.range_eh) === encodeHashToBase64(dimension.range_eh));
-
+              .find((range: Range & { range_eh: EntryHash; }) =>
+                encodeHashToBase64(range.range_eh) === encodeHashToBase64(dimension.range_eh));
+            if(!range) {
+              return {
+                ['dimension-name']: dimension.name,
+                ['range-type']: 'N/A',
+                ['range-min']: 0,
+                ['range-max']: 0,
+              }
+            }
             const [[rangeType, rangeValues]] : any = Object.entries(range?.kind as RangeKind);
-            if(!rangeType || typeof rangeValues !== 'object') throw Error('Error formulating ranges for table record');
+            
             return {
               ['dimension-name']: dimension.name,
               ['range-type']: rangeType,
@@ -192,35 +157,11 @@ export default class DimensionList extends NHComponent {
             }
           });
         this.tableStore.records = tableRecords;
-        console.log('this._dimensionEntries tableRecords:>> ', tableRecords);
+        this.requestUpdate()
       } catch (error) {
         console.log('Error mapping dimensions and ranges to table values: ', error)
       }
     }
-  }
-
-  handleCreateMethod = (dimension: Partial<Dimension> & { range_eh: Uint8Array; }, dimensionIndex: number) => {
-      this._selectedInputDimensionIndex = dimensionIndex;
-
-      const selectedRange = this._rangeEntries
-        .find((range: Range & { range_eh: EntryHash; }) => 
-          encodeHashToBase64(range.range_eh) === encodeHashToBase64(dimension.range_eh));
-
-      // this.dispatchEvent(new CustomEvent("input-dimension-selected", {
-      //   detail: { range: selectedRange, dimensionEh: dimension.dimension_eh },
-      //   bubbles: true,
-      //   composed: true,
-      // }));
-      // this.dispatchEvent(new CustomEvent("request-method-create", {
-      //   detail: {},
-      //   bubbles: true,
-      //   composed: true,
-      // }));
-
-  }
-
-  resetSelectedInputDimensionIndex() {
-    this._selectedInputDimensionIndex = 0;
   }
 
   async connectedCallback() {
@@ -239,6 +180,7 @@ export default class DimensionList extends NHComponent {
         'method-operation': new FieldDefinition<DimensionTableRecord>({heading: 'Type'}),
         'range-min': new FieldDefinition<DimensionTableRecord>({heading: 'Min'}),
         'range-max': new FieldDefinition<DimensionTableRecord>({heading: 'Max'}) }
+
     //@ts-ignore
     this.tableStore = new TableStore({
       tableId: 'dimensions',
@@ -260,67 +202,6 @@ export default class DimensionList extends NHComponent {
     `;
   }
 
-  // private renderInputDimensions(): TemplateResult {
-  //   return typeof this._dimensionEntries == 'undefined' || this._dimensionEntries.length == 0
-  //     ? html`No dimensions available`
-  //     : html`<div style="display:flex; flex-direction: column; gap: 8px;">
-  //       ${this._dimensionEntries.filter((dimension: Dimension) => !dimension.computed)
-  //         .reverse()
-  //         .map((dimension: Dimension & { dimension_eh: EntryHash; }, dimensionIndex: number) => {
-  //           return keyed(encodeHashToBase64(dimension.dimension_eh), html`
-  //             <nh-card 
-  //               class="nested-card ${classMap({
-  //                   selected: this._selectedInputDimensionIndex == dimensionIndex,
-  //                 })}" .theme=${"dark"}
-  //               .heading=${dimension.name}
-  //               .textSize=${"sm"}
-  //             >
-  //               <h1>Range: </h1>
-  //               ${this._rangeEntries?.length
-  //                 ? this.renderRangeDetails(this._rangeEntries
-  //                   .find((range: Range & { range_eh: EntryHash; }) =>
-  //                     encodeHashToBase64(range.range_eh) === encodeHashToBase64(dimension.range_eh))) 
-  //                 : null}
-
-  //               ${this.renderMethodList(dimension, dimensionIndex)}
-  //             </nh-card>`);
-  //         }
-  //         )
-  //       }
-  //     </div>`;
-  // }
-
-  private renderRangeDetails(range: Range & { range_eh: EntryHash } | undefined) : TemplateResult {
-    if(typeof range == "undefined") return html``;
-    return html`
-      <nh-card .theme=${"light"} .title=${range.name} >
-        <h3><em>Kind:</em></h3>
-        <div>${Object.keys(range.kind)[0]}</div>
-        <h3><em>Min:</em></h3>
-        <span>${Object.values(range.kind)[0].min}</span>
-        <h3><em>Max:</em></h3>
-        <span>${Object.values(range.kind)[0].max}</span>
-      </nh-card>
-    `
-  }
-
-  private renderMethodList(dimension: Partial<Dimension> & { range_eh: Uint8Array; } & { dimension_eh: EntryHash; }, dimensionIndex: number): TemplateResult {
-    return this.filteredMethodInputDimensions.length > 0 && this.filteredMethodInputDimensions.some(({ name }) => name === dimension.name)
-      ? html`<h2>Methods using this dimension: </h2>
-              ${this.filteredMethodInputDimensions.map(({ methodEh, name }) => {
-                return name == dimension.name
-                  ? html`${generateHashHTML(methodEh as string)}`
-                  : null;
-              })}`
-      : html`<h1>No methods for this dimension</h1>
-              ${this.renderMethodButton(dimensionIndex, dimension)}`;
-  }
-
-  private renderMethodButton(dimensionIndex: number, dimension: Partial<Dimension> & { range_eh: Uint8Array; } & { dimension_eh: EntryHash; }): TemplateResult {
-    if(this._selectedInputDimensionIndex !== dimensionIndex) { 
-      return html`<nh-button .size=${"sm"} .variant=${"warning"} @click=${() => this.handleCreateMethod(dimension, dimensionIndex)}>Create Method</nh-button>`}
-      else return html`<nh-button .size=${"sm"} .variant=${"warning"} disabled=${this.dimensionSelected}>Creating Method</nh-button>`;
-  }
 
   static elementDefinitions = {
     "nh-button": NHButton,
