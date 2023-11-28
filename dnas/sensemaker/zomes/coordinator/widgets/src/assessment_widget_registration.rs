7e62bde4f850f59444cda41a89c551a91c6cfe07
Nicholas Stebbings <nickolarse@riseup.net>
2023-11-29 09:41:01 +1300

    get_assessment_widget_registrations returns correct result

--- dnas/sensemaker/zomes/coordinator/widgets/src/assessment_widget_registration.rs
+++ dnas/sensemaker/zomes/coordinator/widgets/src/assessment_widget_registration.rs
@@ -59,6 +59,31 @@ fn get_assessment_widget_registration(assessment_widget_registration_eh: EntryHa
     }
 }
 
+#[hdk_extern]
+fn get_assessment_widget_registrations(_:()) -> ExternResult<Vec<Record>> {
+    let links = get_links(
+        registrations_typed_path()?.path_entry_hash()?,
+        LinkTypes::WidgetRegistrations,
+        None,
+    )?;
+    match links.last() {
+        Some(_link) => {
+            let collected_get_results: ExternResult<Vec<Option<Record>>> = links.into_iter().map(|link| {
+                let entry_hash = link.target.into_entry_hash()
+                    .ok_or_else(|| wasm_error!(WasmErrorInner::Guest(String::from("Invalid link target"))))?;
+    
+                get_assessment_widget_registration(entry_hash)
+            }).collect();
+    
+            // Handle the Result and then filter_map to remove None values
+            collected_get_results.map(|maybe_records| {
+                maybe_records.into_iter().filter_map(|maybe_record| maybe_record).collect::<Vec<Record>>()
+            })
+        } 
+        None => Ok(vec![])
+    }
+}
+
 fn registrations_typed_path() -> ExternResult<TypedPath> {
     Path::from("widget_registrations").typed(LinkTypes::WidgetRegistrations)
 }
