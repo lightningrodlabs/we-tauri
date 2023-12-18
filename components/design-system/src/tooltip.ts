c9df0b9ba6cb0b4177fc4a514b1005379e88fbac
Nicholas Stebbings <nickolarse@riseup.net>
2023-12-19 09:48:36 +1300

    update tooltip variant styles

--- components/design-system/src/tooltip.ts
+++ components/design-system/src/tooltip.ts
@@ -8,6 +8,12 @@ export default class NHSlide extends NHComponentShoelace {
   text: string = "Tooltip Text";
   @property()
   visible: boolean = true;
+  @property()
+  variant: "primary"
+  | "success"
+  | "neutral"
+  | "warning"
+  | "danger" = "success";
 
   render() {
     return html`
@@ -15,7 +21,14 @@ export default class NHSlide extends NHComponentShoelace {
         visible: this.visible,
       })}">
         <slot name="hoverable" class="hoverable">Hover</slot>
-        <div class="content">${this.text}</div>
+        <div class="content">
+        <svg class="icon${classMap({
+            [this.variant]: this.variant,
+          })}" width="24" height="24" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+          <path fill-rule="evenodd" clip-rule="evenodd" d="M2 12C2 6.47715 6.47715 2 12 2C17.5229 2 22 6.47715 22 12C22 17.5229 17.5229 22 12 22C6.47715 22 2 17.5229 2 12ZM4 12C4 16.4183 7.58172 20 12 20C16.4183 20 20 16.4183 20 12C20 7.58172 16.4183 4 12 4C7.58172 4 4 7.58172 4 12ZM12.7071 15.2929C13.0976 15.6834 13.0976 16.3166 12.7071 16.7071C12.3166 17.0976 11.6834 17.0976 11.2929 16.7071C10.9024 16.3166 10.9024 15.6834 11.2929 15.2929C11.6834 14.9024 12.3166 14.9024 12.7071 15.2929ZM11 8C11 7.44771 11.4477 7 12 7C12.5523 7 13 7.44772 13 8V13C13 13.5523 12.5523 14 12 14C11.4477 14 11 13.5523 11 13V8Z" fill="currentColor"/>
+        </svg>
+        ${this.text}
+        </div>
       </div>
     `;
   }
@@ -31,17 +44,26 @@ export default class NHSlide extends NHComponentShoelace {
       
       /* Tooltip text */
       .tooltip .content {
-        visibility: hidden;
-        width: 120px;
+        line-height: var(--nh-line-heights-body-relaxed);
+        font-family: var(--nh-font-families-headlines);
+        font-size: calc(1px * var(--nh-font-size-sm));
+        font-weight: var(--nh-font-weights-body-regular);
+
         background-color: var(--nh-theme-bg-detail); 
-        color: #fff;
-        text-align: center;
-        padding: 4px 0;
-        border-radius: 8px;
-        position: absolute;
+        border: 1px solid var(--nh-theme-accent-disabled);
+        color: var(--nh-theme-fg-default); 
+
+        visibility: hidden;
         z-index: 1;
-        top: 100%;
+        position: absolute;
+        top: 105%;
         right: 0%;
+        width: 90%;
+
+        text-align: left;
+        padding: 4px 8px 8px 8px;
+        box-sizing: border-box;
+        border-radius: 8px;
       }
       
       /* Show the tooltip text when you mouse over the tooltip container */
@@ -52,12 +74,35 @@ export default class NHSlide extends NHComponentShoelace {
       .tooltip .content::after {
         content: " ";
         position: absolute;
-        bottom: 99%;  /* At the top of the tooltip */
-        right: 4%;
+        bottom: 100%;  /* At the top of the tooltip */
+        right: 2%;
+        z-index: 2;
         margin-left: -5px;
-        border-width: 5px;
+        border-width: 7px;
         border-style: solid;
-        border-color: transparent transparent var(--nh-theme-bg-detail) transparent;
+        border-color: transparent transparent var(--nh-theme-accent-disabled) transparent;
+      }
+
+      .icon {
+        position: relative;
+        top: 7px;
+        padding-right: 4px;
+      }
+
+      .icon.danger {
+        color: var(--nh-theme-error-default);
+      }
+
+      .icon.success {
+        color: var(--nh-theme-success-default);
+      }
+
+      .icon.primary {
+        color: var(--nh-theme-accent-default);
+      }
+
+      .icon.warning {
+        color: var(--nh-theme-warning-default);
       }
     `,
   ];
