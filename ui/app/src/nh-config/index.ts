a6e5bb77b9055d1196fb550d80b97668960d75b6
Nicholas Stebbings <nickolarse@riseup.net>
2023-12-21 09:30:00 +1300

    move constants to config index

--- ui/app/src/nh-config/index.ts
+++ ui/app/src/nh-config/index.ts
@@ -3,4 +3,9 @@ import CreateDimension from './create-input-dimension-form'
 import CreateOutputDimensionMethod from './create-output-dimension-form'
 import DimensionList from './dimension-list'
 
+export const MIN_RANGE_INT = 0;
+export const MAX_RANGE_INT = 4294967295;
+export const MIN_RANGE_FLOAT = -Number.MAX_SAFE_INTEGER;
+export const MAX_RANGE_FLOAT = Number.MAX_SAFE_INTEGER;
+
 export { NHGlobalConfig, CreateOutputDimensionMethod, CreateDimension, DimensionList }
