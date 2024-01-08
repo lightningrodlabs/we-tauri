c819df1acdecb010e03171840a76d29b3ab78ee4
Jillian Ada Burrows <jill@adaburrows.com>
2024-01-08 17:53:36 -0600

    Update configs adn versions; rebuild; wip

--- ui/libs/styles/build/dark/js/variables.js
+++ ui/libs/styles/build/dark/js/variables.js
@@ -1,6 +1,6 @@
 /**
  * Do not edit directly
- * Generated on Wed, 09 Aug 2023 15:46:33 GMT
+ * Generated on Mon, 08 Jan 2024 23:48:58 GMT
  */
 
 module.exports = {
@@ -1980,23 +1980,6 @@ module.exports = {
           "colors",
           "eggplant",
           "950"
-        ]
-      },
-      "1000": {
-        "value": "#000",
-        "type": "color",
-        "filePath": "dark.json",
-        "isSource": true,
-        "original": {
-          "value": "#000",
-          "type": "color"
-        },
-        "name": "nhColorsEggplant1000",
-        "attributes": {},
-        "path": [
-          "colors",
-          "eggplant",
-          "1000"
         ]
       }
     }
